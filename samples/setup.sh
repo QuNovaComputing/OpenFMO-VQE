2e49b8626f03d368178b64214e1b2e4b12da693a
aidanpellow <aidanpellow@gmail.com>
2023-03-13 16:52:57 +0200

    Delete samples directory

--- samples/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export OMP_NUM_THREADS=48
-export LIBRARY_PATH=$LD_LIBRARY_PATH
-
-# export OFMOPATH=/home/gwonhaklee/OpenFMO # For original version
-export OFMOPATH=/home/gwonhaklee/workspace/OpenFMO
-export SCRDIR=/home/gwonhaklee/workspace/OpenFMO/samples/scratch
